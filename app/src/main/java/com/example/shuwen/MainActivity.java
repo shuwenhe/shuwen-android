d556139ae986f2a2950f964e8c4f5765c82cb14e
shuwen <1201220707@pku.edu.cn>
2024-11-13 14:36:48 +0800

    feat:android shuwen

---
+++ app/src/main/java/com/example/shuwen/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.shuwen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.shuwen.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'shuwen' library on application startup.
+    static {
+        System.loadLibrary("shuwen");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'shuwen' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
